c7bec101bb5ea274d50731160a10d7ccd060bb93
Himanshu <Himanshu.shivhare.9935@gmail.com>
2021-05-30 23:43:04 +0530

    added upto counting timer

--- Activity/script.js
+++ Activity/script.js
@@ -2,7 +2,9 @@
 let videoRecorder = document.querySelector("#record_video");
 let videoElem = document.querySelector("#video_elem");
 let captureBtn = document.querySelector("#capture");
+let timing = document.querySelector("#timing");
 let recordState = false;
+let clearObj;
  let constraint = {
      video : true ,
      audio : true 
@@ -42,12 +44,14 @@ let recordState = false;
  videoRecorder.addEventListener("click" , function(){
      if(recordState == false){
          mediaRecorder.start();
-         videoRecorder.innerHTML = "Recording...";
+         videoRecorder.classList.add("record-animation");
+         startCounting();
          recordState = true;
-     }else{
-         mediaRecorder.stop();
-         videoRecorder.innerHTML = "Record";
-         recordState = false;
+        }else{
+            mediaRecorder.stop();
+            videoRecorder.classList.remove("record-animation");
+            stopCounting();
+            recordState = false;
      }
  })
 
@@ -61,7 +65,7 @@ let recordState = false;
      // draw a frame on that canvas...
      tool.drawImage(videoElem , 0 , 0);
      let link = canvas.toDataURL(); // convert image into url
-
+    captureBtn.classList.add("camera-animation");
      // download code
      let anchor = document.createElement("a");
      anchor.href = link;
@@ -70,4 +74,23 @@ let recordState = false;
      anchor.remove();
      canvas.remove();
 
+
  })
+
+ function startCounting(){
+    timing.classList.add("timing-active");
+    let timeCount = 0;
+    clearObj = setInterval(function(){
+        let second = (timeCount%60)<10 ? `0${timeCount % 60}` : `${timeCount%60}`;
+        let minutes = (timeCount/60)<10 ? `0${Number.parseInt(timeCount/60)}` : `${Number.parseInt(timeCount/60)}`;
+        let hours = (timeCount/3600)<10 ? `0${Number.parseInt(timeCount/3600)}` : `${Number.parseInt(timeCount/3600)}`;
+        timing.innerText = `${hours}:${minutes}:${second}`;
+    timeCount++;
+    } , 1000)
+
+ }
+ function stopCounting(){
+    timing.classList.remove("timing-active");
+    timing.innerText = "00:00:00";
+    clearInterval(clearObj);
+ }
